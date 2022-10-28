eacd5ee844bd747dc43e9c2b440168fff5120263
wwhai <cnwwhai@gmail.com>
2022-10-28 18:16:11 +0800

    dev: add recycle box emu branch

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/binary"
 	"log"
 	"os"
 	"os/signal"
@@ -23,21 +24,29 @@ func main() {
 			return
 		default:
 			{
-
 			}
-
 		}
 		err := server.ListenTCP("0.0.0.0:1502")
 		if err != nil {
 			log.Fatalf("%v\n", err)
 		}
 		server.Debug = true
-		server.Coils = []byte{0, 1}
-		server.DiscreteInputs = []byte{0, 1}
-		server.InputRegisters = []uint16{0, 1}
-		server.HoldingRegisters = []uint16{0, 1}
+		/**/ state_volume := binary.BigEndian.Uint16([]byte{0x02, 0x10}) // 闲暇中，10%容积
+		syn1_syn2 := binary.BigEndian.Uint16([]byte{136, 137})           // SYN = 136+1
+		currentWeight := binary.BigEndian.Uint16([]byte{0x01, 0x10})     // 1.1Kg
+		idCardH := binary.BigEndian.Uint16([]byte{0x01, 0x02})           //
+		idCardL := binary.BigEndian.Uint16([]byte{0x03, 0x04})           // 卡号为1234
+		err1_2 := binary.BigEndian.Uint16([]byte{0x00, 0x00})            // 故障:123
+		err3_ := binary.BigEndian.Uint16([]byte{0x00, 0x00})             // 故障:123
 
-		log.Println("Modbus Server started")
+		server.HoldingRegisters = []uint16{
+			state_volume,
+			syn1_syn2,
+			currentWeight,
+			idCardH, idCardL,
+			err1_2, err3_,
+		}
+		log.Println("Modbus Server started@ 127.0.0.1:1502")
 
 	}(context.Background())
 	defer server.Close()
